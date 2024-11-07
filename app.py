6e8e87963e2381ee8a39578abb346a1331552961
priyasathya <125353483+JPriyasathya@users.noreply.github.com>
2024-11-07 14:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -32,4 +33,4 @@ def predict():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
